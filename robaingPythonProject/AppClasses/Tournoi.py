f25c07f89e394fb8167a037b98830f58d7be281f
robainggg <robincastel2003@gmail.com>
2024-04-03 10:41:47 +0200

    deuxieme type de tournoi

--- robaingPythonProject/AppClasses/Tournoi.py
+++ robaingPythonProject/AppClasses/Tournoi.py
@@ -17,7 +17,7 @@ class Tournoi(Connexion):
             return "Elimination Simple"  # Des tournois à élimination simple: pour 32 joueurs par exemple on aura 16
             # matchs, 16 survivants puis 8 matchs 8 survivants, etc...
         elif nb_joueur < 10:
-            return "rienpourlemoment"  # Un format de tournoi rapide ou tout le monde rencontre tout le monde et la
+            return "Tournoi à la ronde"  # Un format de tournoi rapide ou tout le monde rencontre tout le monde et la
             # personne avec le plus de victoires gagne.
         elif nb_joueur < 32:
             return "rienpourlemoment"  # Comme le tournoi à élimination simple mais certains joueurs auront
@@ -26,7 +26,7 @@ class Tournoi(Connexion):
             return "rienpourlemoment"  # Des poules de 3 à 4 joueurs qui élimineront 1 joueur si
             # 3 joueurs dans la poule et 2 joueurs si 4 joueurs dans la poule.
 
-    def generer_tournoi(self, joueurs : list, nb_joueur: int, nb_table: int, date_heure_debut : datetime):
+    def generer_tournoi(self, joueurs: list, nb_joueur: int, nb_table: int, date_heure_debut: datetime):
         format_tournoi = self.definir_format_tournoi(nb_joueur)
         liste_joueurs = joueurs.copy()
         heure_match = date_heure_debut
@@ -34,15 +34,24 @@ class Tournoi(Connexion):
         if format_tournoi == "Elimination Simple":
             liste_matchs = []
             for i in range(nb_joueur // 2):
-                liste_matchs.append([liste_joueurs.pop(randint(0, liste_joueurs.__len__()-1)), liste_joueurs.pop(randint(0, liste_joueurs.__len__()-1)), heure_match.strftime("%H.%M.%d.%m.%Y")])
+                liste_matchs.append([liste_joueurs.pop(randint(0, liste_joueurs.__len__() - 1)),
+                                     liste_joueurs.pop(randint(0, liste_joueurs.__len__() - 1)),
+                                     heure_match.strftime("%H.%M.%d.%m.%Y")])
                 compteur_table = compteur_table + 1
                 if compteur_table == nb_table:
-                    heure_match = heure_match.replace(year= heure_match.year, month= heure_match.month, day=heure_match.day, hour=heure_match.hour, minute=heure_match.minute + 6)
+                    heure_match += timedelta(minutes=6)  # Ajustement du temps
                     compteur_table = 0
+        elif format_tournoi == "Tournoi à la ronde":
+            liste_matchs = []
+            for i in range(joueurs.__len__()-1):
+                for j in range(i+1, joueurs.__len__() - 1):
+                    liste_matchs.append([joueurs[i], joueurs[j], heure_match.strftime("%H.%M.%d.%m.%Y")])
+                    compteur_table = compteur_table + 1
+                    if compteur_table == nb_table:
+                        heure_match += timedelta(minutes=6)  # Ajustement du temps
+                        compteur_table = 0
+
         return liste_matchs
-
-
-
 
     def calcul_nb_phase(self, nb_joueurs):
         while nb_joueurs > 0:
@@ -51,4 +60,6 @@ class Tournoi(Connexion):
 
 if __name__ == '__main__':
     tournoi = Tournoi()
-    print(tournoi.generer_tournoi(["Robin", "Faraz", "Thomas", "Arthur", "Huseyin", "Thibault", "Sarah", "Esteban"],8, 1, datetime.now()))
+    print(
+        tournoi.generer_tournoi(["Robin", "Faraz", "Thomas", "Arthur", "Huseyin", "Thibault", "Sarah"], 7, 1,
+                                datetime.now()))
