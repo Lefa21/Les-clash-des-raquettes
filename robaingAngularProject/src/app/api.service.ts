992f4964361353f2290a984daae73d828ccac328
amaitre <manbrozer@gmail.com>
2024-06-11 14:47:58 +0200

    djkvnofjvnfeizudcnhbifuezcgnbfhifucnbguifezcnbgiuezbif

--- robaingAngularProject/src/app/api.service.ts
+++ robaingAngularProject/src/app/api.service.ts
@@ -1,23 +1,24 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpEvent, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Player, NewNamePlayer, namePlayer} from "./Player";
+import {namePlayer, NewNamePlayer, Player} from "./Player";
 import {
   AffichageTournament,
-  nameTournament,
-  Tournament,
   Matchs,
+  nameTournament,
   newDateAndHourTournament,
+  Tournament,
   updateTournament
 } from "./Tournament";
-
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
+
   getAffichageJoueur(): Observable<Player[]> {
     return this.http.get<Player[]>(`/api/personnes/`)
   }
@@ -34,38 +35,48 @@ export class ApiService {
     return this.http.get<AffichageTournament[]>('/api/tournois')
   }
 
-  addJoueur(player: Player): Observable<string> {
-    return this.http.post('/api/personnes/inserer_joueur', player, { responseType: 'text' })
+  getChoixFormat(params: any): Observable<any> {
+    let httpParams = new HttpParams();
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        httpParams = httpParams.set(key, params[key].toString());
+      }
+    }
+    return this.http.get<any[]>('/api/tournois/propose_format', { params: httpParams })
   }
 
-  addTournoi(tournament : Tournament): Observable<string>{
-    return this.http.post('/api/tournois/inserer_tournoi', tournament, { responseType: 'text' })
+  addJoueur(player: Player): Observable<string> {
+    return this.http.post('/api/personnes/inserer_joueur', player, {responseType: 'text'})
+  }
+
+  addTournoi(tournament: Tournament): Observable<string> {
+    return this.http.post('/api/tournois/inserer_tournoi', tournament, {responseType: 'text'})
   }
 
   delJoueur(player: namePlayer): Observable<string> {
-    return this.http.post('/api/personnes/supprimer_joueur', player, { responseType: 'text' })
+    return this.http.post('/api/personnes/supprimer_joueur', player, {responseType: 'text'})
   }
 
   delTournament(tournament: nameTournament): Observable<string> {
-    return this.http.post('/api/tournois/supprimer_tournoi', tournament, { responseType: 'text' })
+    return this.http.post('/api/tournois/supprimer_tournoi', tournament, {responseType: 'text'})
   }
 
   sendFile(fichier: File): Observable<string> {
     const formData = new FormData();
     formData.append('fichier', fichier);
-    return this.http.post('/api/personnes/inserer_les_joueurs', formData, { responseType: 'text' });
+    return this.http.post('/api/personnes/inserer_les_joueurs', formData, {responseType: 'text'});
   }
 
   modifierJoueurPseudo(formData: NewNamePlayer): Observable<string> {
-    return this.http.post('/api/personnes/modifier_joueur', formData, { responseType: 'text' });
+    return this.http.post('/api/personnes/modifier_joueur', formData, {responseType: 'text'});
   }
 
   modDateEtHeureTournoi(formData: newDateAndHourTournament): Observable<string> {
-    return this.http.put('/api/tournois/modifier_dateheure_tournoi', formData, { responseType: 'text' });
+    return this.http.put('/api/tournois/modifier_dateheure_tournoi', formData, {responseType: 'text'});
   }
 
-    sendWinner(formData: updateTournament):Observable<string> {
-      return this.http.put('/api/tournois/mettre_a_jour_tournoi', formData, { responseType: 'text' })
-    }
+  sendWinner(formData: updateTournament): Observable<string> {
+    return this.http.put('/api/tournois/mettre_a_jour_tournoi', formData, {responseType: 'text'})
+  }
 }
 
